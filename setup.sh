ae0602dc3f7f87d6845e967ea94e6cdaae58a4f5
Nicolas McCurdy <thenickperson@gmail.com>
2012-03-30 19:31:36 -0400

    fix some syntax stuff in setup.sh (still incomplete)

--- setup.sh
+++ setup.sh
@@ -3,13 +3,17 @@
 cd ~
 git clone https://github.com/thenickperson/configs.git ~/.ghconfigs
 selected_configs=list of all files NOT in {.*,README.md,install.sh,*_examples}
-for item in $selected_configs
+#for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
+#git clone git://github.com/codykrieger/dotvim.git ~/.vim
+#cd ~/.vim
+#rake install
+for item in $(ls -a | grep)
 do
 	if -a "~./"+$item_name
-		mv "."+item_name "."+item_name+".old"
+		mv .$item_name .$item_name.old
 		echo "backup .$item_name.old"
 	fi
-	ln -s repo_dir/item_name ~/.item_name
+	ln -s ~/.ghconfigs/$item_name ~/.$item_name
 	echo "~/."+item_name+" created"
 done
 echo "installation complete"
