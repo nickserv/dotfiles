26227cffca54f8f0f0fffca9db79ce60bb540e68
Nicolas McCurdy <thenickperson@gmail.com>
2012-04-04 03:40:05 -0400

    add awesomebang theme for awesome :)

---
+++ themes/awesomebang/theme.lua
@@ -0,0 +1,99 @@
+-------------------------------
+-- AwesomeBang awesome theme --
+--    by Nicolas McCurdy     --
+-------------------------------
+
+theme = {}
+
+themedir = "~/.config/awesome/themes/awesomebang/"
+
+theme.font          = "sans 8"
+
+-- median color: 3c3d3e
+theme.bg_normal     = "#2f3031"
+theme.bg_focus      = "#6b6c6d"
+theme.bg_urgent     = "#91231c"
+theme.bg_minimize   = "#000000"
+
+theme.fg_normal     = "#828282"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#828282"
+
+theme.border_width  = "1"
+theme.border_normal = "#2f3031"
+theme.border_focus  = "#6b6c6d"
+theme.border_marked = "#91231c"
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Display the taglist squares
+theme.taglist_squares_sel   = themedir .. "taglist/squarefw.png"
+theme.taglist_squares_unsel = themedir .. "taglist/squarew.png"
+
+theme.tasklist_floating_icon = themedir .. "tasklist/floating.png"
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themedir .. "menu/submenu.png"
+theme.menu_height = "15"
+theme.menu_width  = "100"
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = themedir .. "titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = themedir .. "titlebar/close_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = themedir .. "titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = themedir .. "titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themedir .. "titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = themedir .. "titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = themedir .. "titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = themedir .. "titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themedir .. "titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = themedir .. "titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = themedir .. "titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = themedir .. "titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themedir .. "titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = themedir .. "titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = themedir .. "titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themedir .. "titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themedir .. "titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = themedir .. "titlebar/maximized_focus_active.png"
+
+-- You can use your own command to set your wallpaper
+theme.wallpaper_cmd = { "awsetbg " .. themedir .. "wallpaper.png" }
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = themedir .. "layouts/fairhw.png"
+theme.layout_fairv = themedir .. "layouts/fairvw.png"
+theme.layout_floating  = themedir .. "layouts/floatingw.png"
+theme.layout_magnifier = themedir .. "layouts/magnifierw.png"
+theme.layout_max = themedir .. "layouts/maxw.png"
+theme.layout_fullscreen = themedir .. "layouts/fullscreenw.png"
+theme.layout_tilebottom = themedir .. "layouts/tilebottomw.png"
+theme.layout_tileleft   = themedir .. "layouts/tileleftw.png"
+theme.layout_tile = themedir .. "layouts/tilew.png"
+theme.layout_tiletop = themedir .. "layouts/tiletopw.png"
+theme.layout_spiral  = themedir .. "layouts/spiralw.png"
+theme.layout_dwindle = themedir .. "layouts/dwindlew.png"
+
+theme.awesome_icon = themedir .. "icons/awesome16.png"
+
+return theme
