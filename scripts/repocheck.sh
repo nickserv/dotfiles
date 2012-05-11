d0048e7587fba5236a25e80352e646fe43bb914b
Nicolas McCurdy <thenickperson@gmail.com>
2012-05-11 09:34:26 -0400

    clean up repocheck's code and make it actually run git status (not recusive though)

--- scripts/repocheck.sh
+++ scripts/repocheck.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-CWD="$(pwd)" #save starting directory
-ls -1 | while read d
+prev="$(pwd)"
+ls -1 | while read dir
 do
-	[[ ! -d "$d" ]] && continue
-	cd "$d"
-	echo "Changed to directory" $d
-	#Now you can do someting in the sub-directory
-	cd "$CWD" #Change back to old directory
+	[[ ! -d "$dir" ]] && continue
+	cd "$dir"
+	#start git stuff
+	echo $dir
+	git status
+	echo
+	#end git stuff
+	cd "$prev"
 done
