d7e271082064acf9f6e982ad09dff304a42fbbd1
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-14 00:38:30 -0400

    Use better keybindings for making clients fullscreen or floating

--- home/.awesomerc.lua
+++ home/.awesomerc.lua
@@ -263,13 +263,16 @@ globalkeys = awful.util.table.join(
 			mypromptbox[mouse.screen].widget,
 			awful.util.eval, nil,
 			awful.util.getdir("cache") .. "/history_eval")
-		end)
+		end),
+
+	-- Custom
+	awful.key({modkey}, "e", revelation)
 )
 
 clientkeys = awful.util.table.join(
-	awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
+	awful.key({ modkey, "Shift"   }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
 	awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
-	awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
+	awful.key({ modkey,           }, "f",      awful.client.floating.toggle                     ),
 	awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
 	awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
 	awful.key({ modkey, "Shift"   }, "r",      function (c) c:redraw()                       end),
