700d23d36d47d57ee5f4defa46fe717c1e2af9b1
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-14 01:06:35 -0400

    Minor syntax tweak

--- home/.awesomerc.lua
+++ home/.awesomerc.lua
@@ -126,8 +126,7 @@ mytaglist.buttons = awful.util.table.join(
 	awful.button({ }, 3, awful.tag.viewtoggle),
 	awful.button({ modkey }, 3, awful.client.toggletag),
 	awful.button({ }, 4, awful.tag.viewnext),
-	awful.button({ }, 5, awful.tag.viewprev)
-)
+	awful.button({ }, 5, awful.tag.viewprev))
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
 	awful.button({ }, 1, function (c)
