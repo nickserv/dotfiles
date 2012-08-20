d8ae8717cb469f17a3a9db5b76322dacac5dc940
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-20 04:30:45 -0400

    Improve code style in git-update-branch slightly

--- scripts/git-update-branch.sh
+++ scripts/git-update-branch.sh
@@ -1,9 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 current_branch=$(git symbolic-ref -q HEAD)
 current_branch=${current_branch##refs/heads/}
 current_branch=${current_branch:-HEAD}
-if [ $1 ]
-then
+if [ $1 ]; then
 	git checkout $1
 else
 	git checkout gh-pages
