1c9d7c06ff1a06b6ecbcb3498f7b5a01f472a9b5
Nicolas McCurdy <thenickperson@gmail.com>
2012-10-14 00:47:25 -0400

    Make install script more stable

--- install.sh
+++ install.sh
@@ -2,17 +2,18 @@
 
 echo "Installing homeshick..."
 if command -v curl > /dev/null
-	curl -so ~/.homeshick https://raw.github.com/andsens/homeshick/master/homeshick
+	curl -so ~/homeshick https://raw.github.com/andsens/homeshick/master/homeshick
 elseif command -v wget > /dev/null
-	wget -q https://raw.github.com/andsens/homeshick/master/homeshick
-	mv homeshick ~/.homeshick
+	wget -qO ~/homeshick https://raw.github.com/andsens/homeshick/master/homeshick
 else
-	echo "Please install curl or wget."
+	echo "Homeshick installation failed. Please install curl or wget."
+	exit 1
 fi
+chmod a+x ~/homeshick
 
 echo "Installing thenickperson/castle..."
-homeshick clone git@github.com:thenickperson/castle.git
-homeshick symlink thenickperson/castle
+~/homeshick clone git@github.com:thenickperson/castle.git
+~/homeshick symlink thenickperson/castle
 
 echo "Installing thenickperson/dotvim..."
 if command -v curl > /dev/null
@@ -20,6 +21,4 @@ if command -v curl > /dev/null
 elseif command -v wget > /dev/null
 	wget -q https://raw.github.com/thenickperson/dotvim/master/bootstrap.sh
 	bash bootstrap.sh
-else
-	echo "Please install curl or wget."
 fi
