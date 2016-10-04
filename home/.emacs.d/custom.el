83c97ff64c339f9a703b97cc2952268dc08d67c2
Nick McCurdy <thenickperson@gmail.com>
2016-10-04 17:39:55 -0400

    Open links in the OS's default browser

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-default-browser))
  '(package-selected-packages
    (quote
     (js2-mode web-mode helm-projectile helm nyan-mode diff-hl yaml-mode scss-mode rainbow-delimiters projectile markdown-mode magit flycheck emmet-mode))))
