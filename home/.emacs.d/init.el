c2bb60fd1980d8f7deea5e2bd6a963ae1d4762e4
Nick McCurdy <thenickperson@gmail.com>
2016-10-06 22:48:01 -0400

    Add explanatory comments

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,9 +1,13 @@
 ;;; Customize
+;; Most of my configured variables are in a custom file. Packages should be
+;; initialized first because some of the customize variables depend on them.
 (package-initialize)
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
 ;;; Packages
+;; Load package index if it does not exist (new installation) and then install
+;; all selected packages.
 (unless package-archive-contents
   (package-refresh-contents))
 (package-install-selected-packages)
@@ -17,6 +21,7 @@
 (add-to-list 'auto-mode-alist '("\\.s?css\\'" . web-mode))
 
 ;;;; Hooks
+;; Set mode hook variables that don't support customize.
 (add-hook 'css-mode-hook 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'web-mode-hook 'emmet-mode)
@@ -26,16 +31,20 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;;; CLI
+;; Improve mouse support and margin display in terminals.
 (unless window-system
   (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
   (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
   (setq linum-format "%d "))
 
 ;;; Keys
+;; Set magit binds everywhere so it can be launched from dired and other non-
+;; file buffers.
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 ;;;; Helm
+;; Replace bindings for some existing features to use Helm equivalents.
 (define-key global-map [remap find-file] 'helm-find-files)
 (define-key global-map [remap occur] 'helm-occur)
 (define-key global-map [remap list-buffers] 'helm-buffers-list)
