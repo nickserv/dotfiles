35f568e8d93f21fc910278b6f09163bced98fcf1
Nick McCurdy <thenickperson@gmail.com>
2016-10-10 01:25:27 -0400

    Follow symlinks to version controlled files

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -36,6 +36,7 @@
  '(tab-width 2)
  '(text-mode-hook (quote (flyspell-mode)))
  '(tool-bar-mode nil)
+ '(vc-follow-symlinks t)
  '(web-mode-code-indent-offset 2)
  '(web-mode-css-indent-offset 2)
  '(web-mode-markup-indent-offset 2)
