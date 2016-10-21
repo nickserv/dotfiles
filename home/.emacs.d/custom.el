71d1e983ecf0a1a99fde57b29560f4b056284b01
Nick McCurdy <thenickperson@gmail.com>
2016-10-20 23:40:28 -0400

    Put auto save and backup directories in .emacs.d

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/save/" t))))
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backup/"))))
  '(blink-cursor-mode nil)
  '(browse-url-browser-function (quote browse-url-default-browser))
  '(column-number-mode t)
