4bb494e2924e7f98052353f3aeab0de8222db5cc
Nick McCurdy <thenickperson@gmail.com>
2016-12-02 20:34:53 -0500

    Add helm-ag for use in projectile search

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -34,7 +34,7 @@
      ("gnu" . "http://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (smartparens guru-mode transpose-frame exec-path-from-shell js2-mode web-mode helm-projectile helm diff-hl yaml-mode scss-mode rainbow-delimiters projectile markdown-mode magit flycheck emmet-mode)))
+    (helm-ag smartparens guru-mode transpose-frame exec-path-from-shell js2-mode web-mode helm-projectile helm diff-hl yaml-mode scss-mode rainbow-delimiters projectile markdown-mode magit flycheck emmet-mode)))
  '(prog-mode-hook
    (quote
     (rainbow-delimiters-mode flyspell-prog-mode electric-pair-mode)))
