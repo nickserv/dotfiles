5e7c14a3a8ae221e993f4a6a2513c6ccc677e5f8
Nick McCurdy <thenickperson@gmail.com>
2016-12-02 20:50:54 -0500

    Reset emacs font size to default (12)

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -63,4 +63,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Source Code Pro")))))
+ '(default ((t (:family "Source Code Pro")))))
