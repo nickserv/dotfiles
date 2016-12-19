075be9e33f30abe93c4b356c9b1bf2008ba377ac
Nick McCurdy <thenickperson@gmail.com>
2016-12-18 22:41:09 -0500

    Use Company

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -20,6 +20,7 @@
  '(electric-pair-mode t)
  '(fill-column 80)
  '(global-auto-revert-non-file-buffers t)
+ '(global-company-mode t)
  '(global-diff-hl-mode t)
  '(global-flycheck-mode t)
  '(global-linum-mode nil)
@@ -57,7 +58,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (helm-describe-modes helm-descbinds "undo-tree" gist restart-emacs magithub diminish browse-at-remote evil leuven-theme helm-flx super-save transpose-frame exec-path-from-shell js2-mode web-mode helm-projectile helm diff-hl yaml-mode scss-mode rainbow-delimiters projectile markdown-mode magit flycheck emmet-mode)))
+    (company helm-describe-modes helm-descbinds "undo-tree" gist restart-emacs magithub diminish browse-at-remote evil leuven-theme helm-flx super-save transpose-frame exec-path-from-shell js2-mode web-mode helm-projectile helm diff-hl yaml-mode scss-mode rainbow-delimiters projectile markdown-mode magit flycheck emmet-mode)))
  '(prog-mode-hook
    (quote
     (rainbow-delimiters-mode flyspell-prog-mode linum-mode)))
