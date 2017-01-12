0211340179fa9adeeba091aa6d00457d115fbe41
Nick McCurdy <thenickperson@gmail.com>
2017-01-12 08:33:37 -0500

    Use git grep instead of ag

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -58,12 +58,13 @@
      ("gnu" . "https://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (helm-describe-modes helm-descbinds "undo-tree" gist restart-emacs magithub diminish browse-at-remote evil leuven-theme helm-flx super-save helm-ag smartparens transpose-frame exec-path-from-shell js2-mode web-mode helm-projectile helm diff-hl yaml-mode scss-mode rainbow-delimiters projectile markdown-mode magit flycheck emmet-mode)))
+    (helm-describe-modes helm-descbinds "undo-tree" gist restart-emacs magithub diminish browse-at-remote evil leuven-theme helm-flx super-save smartparens transpose-frame exec-path-from-shell js2-mode web-mode helm-projectile helm diff-hl yaml-mode scss-mode rainbow-delimiters projectile markdown-mode magit flycheck emmet-mode)))
  '(prog-mode-hook
    (quote
     (rainbow-delimiters-mode flyspell-prog-mode linum-mode)))
  '(projectile-global-mode t)
  '(projectile-mode t nil (projectile))
+ '(projectile-use-git-grep t)
  '(savehist-mode t)
  '(scroll-bar-mode nil)
  '(sh-basic-offset 2)
