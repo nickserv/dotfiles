157f49cbd730d9d63493cf3b1418883664a3ec11
Nick McCurdy <thenickperson@gmail.com>
2017-01-22 06:30:25 -0500

    Reorganize init code and comment headings

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -7,50 +7,43 @@
 
 ;;; Code:
 
-;;; Customize
-;; Most of my configured variables are in a custom file. Packages should be
-;; initialized first because some of the customize variables depend on them.
+;;; Initial setup
+
+;;;; Customize
+;; Initialize installed packages and enable customized variables.
 (package-initialize)
 (load (setq custom-file (locate-user-emacs-file "custom.el")))
 
-;;; Packages
-;; Load package index and then install all selected packages.
+;;;; Packages
+;; Download package data and install packages selected in customize.
 (package-refresh-contents)
 (package-install-selected-packages)
 
-;;; Set paths on macOS
+;;;; Set paths on macOS
 (when (eq window-system 'ns)
   (exec-path-from-shell-initialize))
 
-;;;; Magithub
-;; Force the package to load after Magit to prevent failure.
-(with-eval-after-load
-    "magit"
-  (require 'magithub)
-  (magithub-feature-autoinject t))
-
-;;; Ivy
-;; Fuzzy matching
-(setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
-(setq ivy-initial-inputs-alist nil)
-
-;;; Modes
-
-;;;; Auto Mode
+;;; Auto mode
+;; Use yaml-mode and replace js-mode with js2-mode.
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
-;;;; Flycheck
-(flycheck-pos-tip-mode)
+;;; Hooks
 
-;;;; Hooks
+;;;; Programming modes
 
-;; Set mode hook variables that don't support customize.
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+(add-hook 'prog-mode-hook 'linum-mode)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; Use Emmet to complete CSS and HTML.
 (add-hook 'css-mode-hook 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode)
 
-;; Ask to save customizations on quit
+;;;; Usability fixes
+
+;; Ask to save customizations on quit.
 (add-hook 'kill-emacs-query-functions 'custom-prompt-customize-unsaved-options)
 
 (defun colorize-compilation-buffer ()
@@ -58,31 +51,12 @@
   (ansi-color-apply-on-region compilation-filter-start (point)))
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
-;; prog-mode
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
-(add-hook 'prog-mode-hook 'linum-mode)
-
 (defun bind-term-paste ()
   "Bind `term-paste'."
   (bind-key "C-c C-y" 'term-paste term-raw-map))
 (add-hook 'term-mode-hook 'bind-term-paste)
 
-;;; Convenience
-(projectile-mode)
-(projectile-discover-projects-in-directory "~/Repos")
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;;; CLI
-;; Improve mouse support and margin display in terminals.
-(unless window-system
-  (bind-keys
-   ("<mouse-4>" . scroll-down-line)
-   ("<mouse-5>" . scroll-up-line))
-  (setq linum-format "%d "))
-
 ;;; Keys
-
 (bind-keys
  ;; Restart
  ("C-x C-S-c" . restart-emacs)
@@ -115,5 +89,35 @@
  ;; Sane Term
  ("C-x t" . sane-term)
  ("C-x T" . sane-term-create))
+
+;;; CLI
+;; Improve mouse support and margin display in terminals.
+(unless window-system
+  (bind-keys
+   ("<mouse-4>" . scroll-down-line)
+   ("<mouse-5>" . scroll-up-line))
+  (setq linum-format "%d "))
+
+;;; Flycheck
+(flycheck-pos-tip-mode)
+
+;;; Ivy
+;; Enable fuzzy matching against any part of candidate strings.
+(setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+(setq ivy-initial-inputs-alist nil)
+
+;;; Magithub
+;; Load after Magit and enable all features.
+(with-eval-after-load
+    "magit"
+  (require 'magithub)
+  (magithub-feature-autoinject t))
+
+;;; Projectile
+(projectile-mode)
+(projectile-discover-projects-in-directory "~/Repos")
+
+;;; Abbreviate yes/no prompts.
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 ;;; init.el ends here
