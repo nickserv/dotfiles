9295572ca6a84a86ca335fd5082964c0f11f6225
Nick McCurdy <thenickperson@gmail.com>
2017-01-22 15:13:30 -0500

    Set multiple hooks and/or functions at once

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -31,14 +31,32 @@
 
 ;;; Hooks
 
+(defun listify (object)
+  "If OBJECT is a list, return it, else wrap it in a list."
+  (if (listp object) object (list object)))
+
+(defun mapflat (function sequence)
+  "Apply FUNCTION to each element of SEQUENCE, and make a list of the results.
+Unlike `mapcar', the list is flattened nondestructively before it is returned."
+  (apply 'append (mapcar function sequence)))
+
 (defmacro add-hooks (&rest args)
   "Call `add-hook' on each cons pair in ARGS.
-For each cons pair, add its `cdr' as a hook function for its `car'."
-  (macroexp-progn (mapcar (lambda (arg)
-                            (let ((hook (car arg))
-                                  (function (cdr arg)))
-                              `(add-hook ',hook ',function)))
-                          args)))
+Each pair has a `car' for setting hooks and a `cdr' for setting
+functions to add to those hooks.  Either side of the pair can be
+a single symbol or a list of symbols, in which case a function
+can be added to multiple hooks and/or multiple functions can be
+added to a hook."
+  (macroexp-progn
+   (mapflat (lambda (arg)
+              (let ((hooks (listify (car arg)))
+                    (functions (listify (cdr arg))))
+                (mapflat (lambda (hook)
+                           (mapcar (lambda (function)
+                                     `(add-hook ',hook ',function))
+                                   functions))
+                         hooks)))
+            args)))
 
 (defun colorize-compilation-buffer ()
   "Use ANSI colors for compilation."
@@ -52,13 +70,10 @@ For each cons pair, add its `cdr' as a hook function for its `car'."
 
 (add-hooks
  ;; Programming modes
- (prog-mode-hook . flyspell-prog-mode)
- (prog-mode-hook . linum-mode)
- (prog-mode-hook . rainbow-delimiters-mode)
+ (prog-mode-hook . (flyspell-prog-mode linum-mode rainbow-delimiters-mode))
 
  ;; Use Emmet to complete CSS and HTML.
- (css-mode-hook . emmet-mode)
- (sgml-mode-hook . emmet-mode)
+ ((css-mode-hook sgml-mode-hook) . emmet-mode)
 
  ;; Usability fixes
  (kill-emacs-query-functions . custom-prompt-customize-unsaved-options)
