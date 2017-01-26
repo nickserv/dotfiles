c4b797b72222262f511923e2de6a93260d733398
Nick McCurdy <thenickperson@gmail.com>
2017-01-26 01:29:28 -0500

    Reinstall Tern for completion through completion-at-point-functions

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -57,7 +57,7 @@
      ("gnu" . "https://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (smex bind-key flx counsel sane-term flycheck-pos-tip undo-tree gist restart-emacs magithub browse-at-remote leuven-theme super-save exec-path-from-shell yaml-mode scss-mode projectile markdown-mode magit flycheck emmet-mode)))
+    (tern smex bind-key flx counsel sane-term flycheck-pos-tip undo-tree gist restart-emacs magithub browse-at-remote leuven-theme super-save exec-path-from-shell yaml-mode scss-mode projectile markdown-mode magit flycheck emmet-mode)))
  '(projectile-completion-system (quote ivy))
  '(savehist-mode t)
  '(scroll-bar-mode nil)
