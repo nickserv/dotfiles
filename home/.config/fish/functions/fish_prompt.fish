afcbeda6e94e43b4d364616353f42134ba823584
Nicky McCurdy <nick@nickmccurdy.com>
2026-07-13 14:13:38 -0400

    Shorten Google Drive paths in prompt for readability

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,11 @@
 function fish_prompt
     set -g fish_prompt_pwd_dir_length 0
+
+    # Shorten Google Drive paths for readability
+    set -l real_pwd (prompt_pwd | string replace "~/Library/CloudStorage/GoogleDrive-thenickperson@gmail.com/My Drive" "\$DRIVE")
+
     set_color $fish_color_cwd
-    echo -n (prompt_pwd)
+    echo -n $real_pwd
     set_color $fish_color_quote
     echo -n ' → '
 end
